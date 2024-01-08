b34886ec31be2e3135cb5847852d0ede1caf4d42
Mesh <harmeshuppal@gmail.com>
2024-01-08 23:41:34 +0000

    Fixes after testing

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
 @section('sidebar')
  
 @section('content')
-    <div class="flex flex-wrap gap-10 justify-between after:content-[''] after:flex-auto">
+    <div class="flex flex-wrap gap-10 justify-between after:content-[''] after:flex-auto w-full">
         @foreach ($sites as $site)
             <a href="{{ $site['url'] }}" target=”_blank” class="flex inline-block w-72 h-60 flex-col bg-slate-50 rounded-lg p-6 border border-gray-300 shadow-lg">
                 <figure>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,9 +18,9 @@
         <div class="w-full max-w-7xl m-[auto] px-10">
             <header class="pt-6 pb-6">
                 <h1 class="text-slate-900 font-extrabold text-4xl tracking-tight">DevSites</h1>
-                <div class="mt-2 text-lg">Technologies and services related to PHP / Javascript development</div>
+                <div class="mt-2 text-lg">Technologies and services related to PHP development</div>
             </header>
-            <main class="flex flex-row pb-20">
+            <main class="flex flex-row pb-20 w-[960px]">
                 @yield('content')
                 @section('sidebar')
                     <div class="w-60 pl-10">
